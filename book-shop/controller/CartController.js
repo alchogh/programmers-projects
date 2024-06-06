9254c5a26ca0e714608e85d85c433005cfece081
alchogh <alchogh01@gmail.com>
2024-06-06 16:44:44 +0900

    🎨 header token 가져오기

--- book-shop/controller/CartController.js
+++ book-shop/controller/CartController.js
@@ -1,13 +1,29 @@
+const jwt = require("jsonwebtoken");
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
+const dotenv = require("dotenv");
+dotenv.config();
+
+function ensureAuthorization(req, res) {
+	try {
+		let receivedJwt = req.headers["authorization"];
+		let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+		return decodedJwt;
+	} catch (err) {
+		console.log(err.name);
+		console.log(err.message);
+		return err;
+	}
+}
 
 //장바구니 담기
 const addToCart = (req, res) => {
-	const { user_id, book_id, quantity } = req.body;
-	//좋아요 추가
+	const { book_id, quantity } = req.body;
+	let authorization = ensureAuthorization(req);
+
 	let sql =
 		"INSERT INTO cartItems (user_id, book_id, quantity) VALUES (?, ?,?)";
-	let values = [user_id, book_id, quantity];
+	let values = [authorization.id, book_id, quantity];
 
 	conn.query(sql, values, (err, results) => {
 		if (err) {
@@ -19,34 +35,38 @@ const addToCart = (req, res) => {
 
 //장바구니 아이템 목록 조회
 const getCartItems = (req, res) => {
-	const { user_id, selected } = req.body;
-	let sql =
-		"SELECT cartItems.id, book_id, title, summary, quantity, price FROM cartItems LEFT JOIN books ON cartItems.book_id=books.id WHERE user_id=? AND cartItems.id IN (?)";
-	let values = [user_id, selected];
-	conn.query(sql, values, (err, results) => {
-		if (err) {
-			return res.status(StatusCodes.BAD_REQUEST).end();
-		}
-		return res.status(StatusCodes.OK).json(results);
-	});
+	const { selected } = req.body;
+	let authorization = ensureAuthorization(req, res);
+
+	if (authorization instanceof jwt.TokenExpiredError) {
+		return res
+			.status(StatusCodes.UNAUTHORIZED)
+			.json({ message: "다시 로그인 해라" });
+	} else {
+		let sql =
+			"SELECT cartItems.id, book_id, title, summary, quantity, price FROM cartItems LEFT JOIN books ON cartItems.book_id=books.id WHERE user_id=? AND cartItems.id IN (?)";
+		let values = [authorization.id, selected];
+		conn.query(sql, values, (err, results) => {
+			if (err) {
+				return res.status(StatusCodes.BAD_REQUEST).end();
+			}
+			return res.status(StatusCodes.OK).json(results);
+		});
+	}
 };
 
 //장바구니 삭제
 const removeCartItems = (req, res) => {
-	const { id } = req.params;
+	const cartItemId = req.params.id;
 
 	let sql = "DELETE FROM cartItems WHERE id = ?";
 
-	conn.query(sql, id, (err, results) => {
+	conn.query(sql, cartItemId, (err, results) => {
 		if (err) {
 			return res.status(StatusCodes.BAD_REQUEST).end();
 		}
 		return res.status(StatusCodes.OK).json(results);
 	});
-};
-
-const select = (req, res) => {
-	res.json("장바구니 조회");
 };
 
 module.exports = { addToCart, getCartItems, removeCartItems };
--- book-shop/controller/LikesController.js
+++ book-shop/controller/LikesController.js
@@ -1,12 +1,23 @@
+const jwt = require("jsonwebtoken");
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
+const dotenv = require("dotenv");
+dotenv.config();
 
+function ensureAuthorization(req) {
+	let receivedJwt = req.headers["authorization"];
+	let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+	return decodedJwt;
+}
+
+//좋아요 추가
 const addLike = (req, res) => {
-	const { id } = req.params;
-	const { user_id } = req.body;
-	//좋아요 추가
+	const book_id = req.params.id;
+
+	let authorization = ensureAuthorization(req);
+
 	let sql = "INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)";
-	let values = [user_id, id];
+	let values = [authorization.id, book_id];
 
 	conn.query(sql, values, (err, results) => {
 		if (err) {
@@ -17,10 +28,12 @@ const addLike = (req, res) => {
 };
 
 const removeLike = (req, res) => {
-	const { id } = req.params;
-	const { user_id } = req.body;
+	const book_id = req.paramsid;
+
+	let authorization = ensureAuthorization(req);
+
 	let sql = "DELETE FROM likes WHERE user_id=? AND liked_book_id=?";
-	let values = [user_id, id];
+	let values = [authorization.id, book_id];
 	conn.query(sql, values, (err, results) => {
 		if (err) {
 			return res.status(StatusCodes.BAD_REQUEST).end();
